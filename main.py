f7f4066edd3d6cb99ed4a4647ad1deded46587e2
Ilyas <194538383+i77-coffee@users.noreply.github.com>
2025-01-14 21:16:06 +0300

    Add some starter code

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello, World!")
+import random
+
+def generatePassword(smartPassword):
+  password = ""
+
+  if smartPassword == "Y":
+    print("Smart password in being used")
+  elif smartPassword == "N":
+    print("Smart password in not being used, regular password is")
+  else:
+    print("Please choose a viable option...")
+
+  return password
+
+useSmartPassword = str(input("Generate a smart password [Y] or [N]: "))
+
+generatePassword(useSmartPassword)
